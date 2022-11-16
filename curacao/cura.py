258717a6fc3108d84cb0e056ed94b0115e4e235b
jechs83 <jechs83@gmail.com>
2022-11-16 06:20:20 -0500

    se actualizo  el update de la fecha y hora en cura y saga faltan los demas

--- curacao/cura.py
+++ curacao/cura.py
@@ -9,29 +9,32 @@ import time
 import json
 from bd_record import save_data_to_mongo_db
 from decouple import config
-server_date = datetime.now()
-timezone = pytz.timezone("America/Bogota")
-peru_date = server_date.astimezone(timezone)
-current_date = peru_date.strftime("%d/%m/%Y" )
-current_time =peru_date.strftime("%H:%M" )
+from datetime import datetime
+from datetime import date
 
 web_url = random.choice(config("PROXY"))
 web_url = "http://"+web_url
-
 client = MongoClient(config("MONGO_DB"))
 
 
 print(web_url)
  
+def load_datetime():
+    
+ today = date.today()
+ now = datetime.now()
+ date_now = today.strftime("%d/%m/%Y")  
+ time_now = now.strftime("%H:%M:%S")
+ return date_now, time_now
 
 def scrap (web):
  
     proxies = {"http":web_url }
     #res=requests.get(web,  proxies= proxies)
-    HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
-
+    #HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
+    res=requests.get(web,  proxies= proxies)
     
-    res = requests.get(web, proxies={"http": web_url}, headers = HEADERS)
+    #res = requests.get(web, proxies={"http": web_url}, proxies = proxies)
 
     
     server = "Respuesta del servidor :"+str(res.status_code)
@@ -47,17 +50,20 @@ def scrap (web):
         return True
     
     for i in product:
+
+    
         product = i.find("a")["title"]
-        print(product)
+        #print(product)
         brand = i.find("div", class_="Manufacturer").text
-        print(brand)
+        
+        #print(brand)
         try:
             sku = i.find("div" ,class_="PartNumber" ).text
             sku = sku.replace("_P","")
         except: sku = None
         
         link = i.find("a")["href"]
-        print(link)
+        #print(link)
 
         try:
             image = i.find("img")["data-src"]
@@ -83,38 +89,26 @@ def scrap (web):
 
         except: web_dsct = 0
         web_dsct = round( web_dsct)
-        market = "curacao"
-        
-        db = client["curacao"]
-        collection = db["market"]
-        db_max = client["scrap"]
-        collection_max = db_max["scrap"]
+      
+      
 
         try:
             card_price = 0
         except:card_price=0
-        #print(card_price)
-        bd_name_store = market
-        dsct = web_dsct
 
+        bd_name_store = "curacao"
+        collection = "market"
+        dsct = web_dsct
+        market = "curacao"
         card_dsct = 0
-        save_data_to_mongo_db(bd_name_store, market,sku,brand,product,list_price,
-                            best_price,card_price,link,image,dsct, card_dsct)
+        date_time = load_datetime()
+
+
+        save_data_to_mongo_db(bd_name_store,collection, market,sku,brand,product,list_price,
+                            best_price,card_price,link,image,dsct, card_dsct, date_time[0] ,date_time[1])
+
         
 
-# def scrap_category(web1,web2,count):
-#     for i in range(500):
-       
-#         success = scrap(web1+str((i+1)*count)+web2)
-#         print(web1+str((i+1)*count)+web2)
-#         #time.sleep(3)
-#         if success == True:
-#             print("succes es Verdadero ")
-#             return False
-
-array_tec=[]
-
-#arg_ = sys.argv[1]
 num = sys.argv[1]
 arg_ = config("CURACAO_TEXT_PATH")+str(num)+".txt"
 
@@ -124,21 +118,8 @@ x = f.readlines()
 with open(arg_) as load_file:
     data = [tuple(line.split()) for line in load_file]
 
-
-
-# for id, val in enumerate(data):
-    
-#         count = 12
-#         web1 = val[0]
-#         #web2=val[1]
-#         web2 = "&pageSize=12&pageGroup=Category&urlLangId=-24"
-        
-#         scrap_category(web1,web2,count) ## GENERA LA LISTA DE PAGINACIONES POR CATEGORIA
-#         if scrap_category == False:
-#             continue
-
-
-counter =len(array_tec)
+counter =len(data)
+print(counter)
 def curacaro_scrap():
 
     for id, val in enumerate(data):
@@ -151,11 +132,15 @@ def curacaro_scrap():
             for i in range(500):
        
                 success = scrap(web1+str((i+1)*count)+web2)
-                print(web1+str((i+1)*count)+web2)
+                if i == 1:
+
+                         current_url = web1+str((i+1)*count)+web2
+                         print(current_url)
                 #time.sleep(3)
                 if success == True:
-                    print("se termino pahginacion")
+                    print("se termino paginacion continua con la siguiente ")
                     break
+            
 
             if id == counter-1:
                 print("se acabo la web y va comenzar a dar vueltas")
