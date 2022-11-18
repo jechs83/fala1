48d22ed1731ff811617c872b93b77b4d21b01822
jechs83 <jechs83@gmail.com>
2022-11-18 12:56:44 -0500

    buscador

--- buscador/bot_excelsior.py
+++ buscador/bot_excelsior.py
@@ -7,13 +7,12 @@ import sys
 from telegram import message
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from search_bot_service import busqueda, search_brand_dsct, auto_telegram, delete_brand,add_brand_list,read_brands,manual_telegram
-TOKEN = config("CAPITAN_SPOK_TOKEN")
-chat_ide = config("DISCOVERY_CHAT_TOKEN")
-bot_token = config("CAPITAN_SPOK_TOKEN")
+TOKEN = config("CAPITAN_PIKE_TOKEN")
+chat_ide = config("EXCELSIOR_CHAT_TOKEN")
+bot_token = config("CAPITAN_PIKE_TOKEN")
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s -  %(message)s,"
-)
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s -  %(message)s,")
 logger = logging.getLogger()
 
 
@@ -21,12 +20,13 @@ def getBotInfo(update, context):
     bot = context.bot
     chatId= update.message.chat_id
     userName = update.effective_user["first_name"]
-    logger.info(f"el usuario {userName} ha solicitado informacion sobre el bot "+str(chatId))
+    logger.info(f"el usuario {userName} ha solicitado informacion sobre el bot " +str(chatId))
     print(context.args)
+    
     bot.sendMessage(
         chat_id=chatId,
         parse_mode="HTML",
-        text= f"Hola soy un bot creado para la Nave de Discovery. Sigo Funcionando no te preocupes"
+        text= f"Hola soy un bot creado para la Nave Excelsior. sigo funcionando no te preocupes "
     )
 
 def welcomeMsg(update, context):
@@ -36,7 +36,7 @@ def welcomeMsg(update, context):
     for user in updateMsg.new_chat_members:
         userName = user.first_name
     
-    logger.info(f"El usuario {userName} ha ingresado al grupo")
+    logger.info(f"El usuario {userName} ha ingresado al grupo" )
 
     bot.sendMessage(
         chat_id= chatId,
@@ -54,12 +54,12 @@ def custom_search(update, context):
     dsct=int(context.args[1])
     if dsct <= 41:
        dsct = 40
-    search_brand_dsct(str(brand), dsct,bot_token,chat_ide)
+    search_brand_dsct(brand, dsct, bot_token,chat_ide)
 
     bot.sendMessage(
         chat_id=chatId,
         parse_mode="HTML",
-        text= f"Se realizo busqueda de la marca ingresada"+ str(brand) +" de "+ str(dsct) + "%  a mas\n\n#####################################\n#####################################"
+        text= f"Se realizo busqueda de la marca ingresada "+ str(brand) +" de "+ str(dsct) + "%  a mas\n\n#####################################\n#####################################"
     )
 
 
@@ -83,7 +83,7 @@ def sku(update, context):
     logger.info(f"el usuario {userName}  busca codigo especifico")
     codigo = context.args[0]
     
-    busqueda(str(codigo), bot_token, chat_ide)
+    busqueda(str(codigo),bot_token,chat_ide)
     bot.sendMessage(
         chat_id=chatId,
         parse_mode="HTML",
@@ -91,12 +91,11 @@ def sku(update, context):
     )
 
 
-
 def auto_tele(update, context):
     bot = context.bot
     chatId= update.message.chat_id
     userName = update.effective_user["first_name"]
-    logger.info(f"el usuario {userName} ha solicitado una busqueda")
+    logger.info(f"el usuario {userName}  busqueda automatica")
     category=str(context.args[0])
     bot.sendMessage(
         chat_id=chatId,
@@ -104,17 +103,17 @@ def auto_tele(update, context):
         text= f"Espera un momento se esta procesando la solicitud "
     )
     
-    auto_telegram( category,"scrap","excelsior1","excelsior2" ,bot_token,chat_ide)
+    auto_telegram( category,"scrap","excelsior1", "excelsior2" ,bot_token,chat_ide)
 
     bot.sendMessage(
         chat_id=chatId,
         parse_mode="HTML",
         text= f"Se termino la busqueda "
-        )
+    )
     logger.info(f"se Termino la Busqueda")
-
-
     
+
+
 
 ###########################################################################
 
@@ -182,9 +181,6 @@ def auto_tele_dsct(update, context):
 
 
 
-
-
-
 if __name__ == "__main__":
     myBot = telegram.Bot(token = TOKEN)
     print(myBot.getMe())
@@ -197,11 +193,13 @@ dp= updater.dispatcher
 dp.add_handler(CommandHandler("botinfo", getBotInfo))
 dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, welcomeMsg))
 
-#dp.add_handler(MessageHandler( Filters.text, echo))
+
 try:
  dp.add_handler(CommandHandler('b', custom_search))
 except:
     print("esta corriendo")
+
+
 dp.add_handler(CommandHandler('alert', alert_all))
 
 dp.add_handler(CommandHandler('cod', sku))
@@ -209,14 +207,12 @@ dp.add_handler(CommandHandler('cod', sku))
 dp.add_handler(CommandHandler('auto', auto_tele))
 
 dp.add_handler(CommandHandler('manual', auto_tele_dsct))
+
 ###############################
 
 dp.add_handler(CommandHandler('brand', add_brand))
 dp.add_handler(CommandHandler('delete', brand_delete))
 dp.add_handler(CommandHandler('cat', brands_list))
-
-
-
 
 updater.start_polling()
 updater.idle()
