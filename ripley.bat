148965ff2720bc87c7886c1c54376978956564b3
jechs <jechs83@gmail.com>
2022-11-23 16:36:24 -0500

    windowsupdate

--- ripley.bat
+++ ripley.bat
@@ -1,3 +1,4 @@
 start python  C:\GIT\fala\ripley\ripley.py 0
 start python  C:\GIT\fala\ripley\ripley.py 1
 start python  C:\GIT\fala\ripley\ripley.py 2
+start python  C:\GIT\fala\ripley\ripley.py 99
