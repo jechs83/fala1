0e0ebaf4acd62d715fa591d2f6eb4ea33a1f20de
jechs83 <jechs83@gmail.com>
2022-12-21 02:37:51 -0500

    bar promart

--- promart.bat
+++ promart.bat
@@ -1,7 +1,5 @@
-start python  C:\GIT\fala\promart\promart.py  1
-start python  C:\GIT\fala\promart\promart.py  2
-start python  C:\GIT\fala\promart\promart.py  3
-start python  C:\GIT\fala\promart\promart.py  4
-start python  C:\GIT\fala\promart\promart.py  5
-
-
+start C:\GIT\fala\zbatch_files\promart1.bat
+start C:\GIT\fala\zbatch_files\promart2.bat
+start C:\GIT\fala\zbatch_files\promart3.bat
+start C:\GIT\fala\zbatch_files\promart4.bat
+start C:\GIT\fala\zbatch_files\promart5.bat
