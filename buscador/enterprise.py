0976fc64fcdbc6e898cbdd93947229c1d795f236
jechs83 <jechs83@gmail.com>
2022-12-27 14:34:18 -0500

    buscador

--- buscador/enterprise.py
+++ buscador/enterprise.py
@@ -16,7 +16,7 @@ from bot_unique import super_bot
 TOKEN = config("ENTERPRISE_TOKEN")
 chat_id = config("ENTERPRISE_CHAT_TOKEN")
 bot_token = config("ENTERPRISE_TOKEN")
-bd1 = "enterprise1"
+db1 = "enterprise1"
 db2 = "enterprise2"
 
 super_bot(TOKEN, bot_token, chat_id,db1,db2)
