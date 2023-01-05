f948ec01a0501cd4d94796ffe4facf15967af57b
jechs83 <jechs83@gmail.com>
2023-01-05 10:02:00 -0500

    buscador

--- buscador/auto_search.py
+++ buscador/auto_search.py
@@ -4,6 +4,7 @@ from decouple import config
 import telegram
 import logging
 import sys
+import time
 from telegram import message
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from search_bot_service import  auto_telegram
@@ -16,18 +17,31 @@ bot_token = config("CAPITAN_SPOK_TOKEN")
 bd1 = "discovery1"
 bd2 = "discovery2"
 
-auto_telegram("electro", bd1,bd2,bot_token, chat_id)
+def hora():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
 
-auto_telegram("tecno", bd1,bd2,bot_token, chat_id)
+def buscador():
+    auto_telegram("electro", bd1,bd2,bot_token, chat_id)
 
-auto_telegram("juguetes", bd1,bd2,bot_token, chat_id)
+    auto_telegram("tecno", bd1,bd2,bot_token, chat_id)
 
-auto_telegram("ropa", bd1,bd2,bot_token, chat_id)
+    auto_telegram("juguetes", bd1,bd2,bot_token, chat_id)
 
-auto_telegram("bicicleta", bd1,bd2,bot_token, chat_id)
+    auto_telegram("ropa", bd1,bd2,bot_token, chat_id)
 
-auto_telegram("celular", bd1,bd2,bot_token, chat_id)
+    auto_telegram("bicicleta", bd1,bd2,bot_token, chat_id)
 
-auto_telegram("herramientas", bd1,bd2,bot_token, chat_id)
+    auto_telegram("celular", bd1,bd2,bot_token, chat_id)
+
+    auto_telegram("herramientas", bd1,bd2,bot_token, chat_id)
+
+    print("se pausa 10 min")
+    print(hora())
+    time.sleep(10*60) #this will stop the program for 10 minutes
+
+    buscador()
 
 
+buscador()
