307a810028b1e7c889b2e11fb846c692c0f76d93
jchung83 <jchung@sparxworks.com>
2023-01-26 09:44:43 -0500

    buscador

---
+++ buscador/40a.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from telegram import ParseMode
+from decouple import config
+import telegram
+import logging
+import sys
+import time
+from telegram import message
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from search_bot_service import  auto_telegram, auto_telegram_total
+import gc
+
+TOKEN = config("CAPITAN_SPOK_TOKEN")
+chat_id = config("DISCOVERY_CHAT_TOKEN")
+bot_token = config("CAPITAN_SPOK_TOKEN")
+bd1 = "discovery1"
+bd2 = "discovery2"
+dsct = 60
+
+def hora():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
+
+def buscador():
+    auto_telegram_total( bd1,bd2,bot_token, chat_id, dsct)
+
+buscador()
---
+++ buscador/auto_search_excelsiora.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from telegram import ParseMode
+from decouple import config
+import telegram
+import logging
+import sys
+import time
+from telegram import message
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from search_bot_service import  auto_telegram ,auto_telegram_total
+import gc
+
+
+
+TOKEN = config("CAPITAN_PIKE_TOKEN")
+chat_id = config("EXCELSIOR_CHAT_TOKEN")
+bot_token = config("CAPITAN_PIKE_TOKEN")
+bd1 = "excelsior1"
+bd2 = "excelsior2"
+porcentage = 70
+
+def hora():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
+
+def buscador():
+    auto_telegram_total( bd1,bd2,bot_token, chat_id, porcentage)
+
+
+buscador()
