b00136232afe60751f27131597020141fdef4a56
jchung83 <jchung@sparxworks.com>
2023-01-30 12:45:08 -0500

    busacodr

--- buscador/c.py
+++ buscador/c.py
@@ -14,6 +14,7 @@ def start():
     subprocess.Popen([ "start", "cmd" , "/k",  "C:\Git\\fala\\buscador\\auto_search_excelsior.py"], shell=True, executable="C:\windows\system32\cmd.exe")
     subprocess.Popen([ "start", "cmd" , "/k",  "C:\Git\\fala\\buscador\\discovery.py"], shell=True, executable="C:\windows\system32\cmd.exe")
     subprocess.Popen([ "start", "cmd" , "/k", "C:\Git\\fala\\buscador\\enterprise.py"], shell=True, executable="C:\windows\system32\cmd.exe")
+    subprocess.Popen([ "start", "cmd" , "/k", "C:\Git\\fala\\buscador\\.py"], shell=True, executable="C:\windows\system32\cmd.exe")
     time.sleep(20*60)
     subprocess.run(["taskkill", "/IM", "cmd.exe", "/F"])
     subprocess.run(["taskkill", "/IM", "py.exe", "/F"])
