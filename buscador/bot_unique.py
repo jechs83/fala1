09b193b93cd566da17039fa9d0f2197882a1660f
jechs83 <jechs83@gmail.com>
2023-04-23 02:21:53 -0500

    update

--- buscador/bot_unique.py
+++ buscador/bot_unique.py
@@ -4,6 +4,8 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 from register import     register_safa
+import unicodedata
+import re
 
 
 from search_bot_service import busqueda, search_brand_dsct, auto_telegram, delete_brand,add_brand_list,read_category,manual_telegram, search_market_dsct,search_market2_dsct, search_product_dsct_html, test2, search_brand_dsct_html,read_brands,search_price
@@ -497,37 +499,76 @@ def super_bot(TOKEN, bot_token ,chat_id, db1,db2):
 
         name = (var[0])
         last_name= (var[1])
+
+ 
+        normalizada = unicodedata.normalize('NFKD', name).encode('ASCII', 'ignore').decode('utf-8')
+        normalizada2 = unicodedata.normalize('NFKD', last_name).encode('ASCII', 'ignore').decode('utf-8')
+        if normalizada.isalpha() and normalizada2.isalpha():
+            print("La cadena contiene solo letras (sin acentos)")
+        else:
+           bot.sendMessage(
+                chat_id=chat_id,
+                parse_mode="HTML",text= f"Formato incorrecto: /f nombre appelido celular email"
+                )
+           name = None;last_name = None
+
+
+       
         #dni=str(var[2])
         cel=str(var[2])
-        email=str(var[3])
-        #pwd=str(var[5])
-
-        if user_id == 1160667522 or 1712594729:
-            bot.sendMessage(
-                chat_id=chat_id,
-                parse_mode="HTML",text= f"Creando cuenta Fazil"
-        
-                )
-            try:
-                register_safa(name,last_name,cel,email)
-                bot.sendMessage(
-                chat_id=chat_id,
-                parse_mode="HTML",text= f"Usuario se creo exitosamente "
-                )
-            except:
-    
-        
-                bot.sendMessage(
-                chat_id=chat_id,
-                parse_mode="HTML",text= f"Usuario ya existe o hubo error en el proceso "
-        
-                )
+        if cel.isdigit() and len(cel) == 9:
+            print("The string contains only numbers and 9 digits")
         else:
             bot.sendMessage(
                 chat_id=chat_id,
-                parse_mode="HTML",text= f"Acceso Restringido, no estas autorizado a usar este comando"
-        
+                parse_mode="HTML",text= f"Formato incorrecto: /f nombre appelido celular email, el celular solo debe tener 9 digitos"
                 )
+            cel = None
+            
+
+
+        email=str(var[3])
+        pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+        if re.match(pattern, email):
+            print("La dirección de correo electrónico es válida")
+        else:
+            bot.sendMessage(
+                chat_id=chat_id,
+                parse_mode="HTML",text= f"Formato incorrecto: /f nombre appelido celular email, formato de email incorrecto"
+                )
+            email = None
+            
+        #pwd=str(var[5])
+        if name or last_name or cel or email == None:
+            print(" error de data input")
+        if name and last_name and cel and email != None:
+            if user_id == 1160667522 or 1712594729:
+                bot.sendMessage(
+                    chat_id=chat_id,
+                    parse_mode="HTML",text= f"Creando cuenta Fazil"
+            
+                    )
+                try:
+                    register_safa(name,last_name,cel,email)
+                    bot.sendMessage(
+                    chat_id=chat_id,
+                    parse_mode="HTML",text= f"Usuario se creo exitosamente "
+                    )
+                except:
+        
+            
+                    bot.sendMessage(
+                    chat_id=chat_id,
+                    parse_mode="HTML",text= f"Usuario ya existe o hubo error en el proceso "
+            
+                    )
+            else:
+                bot.sendMessage(
+                    chat_id=chat_id,
+                    parse_mode="HTML",text= f"Acceso Restringido, no estas autorizado a usar este comando"
+            
+                    )
 
 
       
