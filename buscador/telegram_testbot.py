0f3808dd312e32010939f5e4d1345a7c2f6615af
jechs83 <jechs83@gmail.com>
2023-10-06 03:46:36 -0500

    update caraviable

---
+++ buscador/telegram_testbot.py
@@ -0,0 +1,24 @@
+
+from search_bot_service import  auto_telegram_category
+from pymongo import MongoClient
+from decouple import config
+
+client = MongoClient(config("MONGO_DB"))
+chat_id = config("R2D2_CHAT")
+bot_token = config("R2D2_TOKEN")
+bd1 = "nameless1"
+bd2 = "nameless2"
+dsct = 80
+dsct2 = 100
+product  = "lentes"
+category = alterno
+
+db="scrap"
+db_collection = "scrap"
+def buscador():
+    auto_telegram_category( category, bd1,bd2,bot_token, chat_id,dsct)
+        
+    buscador() 
+    
+
+buscador()
