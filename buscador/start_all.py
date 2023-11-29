f6ca4fefd7a981728bac9e475c518d8d6fd5ea26
jechs83 <jechs83@gmail.com>
2023-11-28 23:21:00 -0500

    Sas

--- buscador/start_all.py
+++ buscador/start_all.py
@@ -22,7 +22,7 @@ def start():
     # subprocess.Popen([ "start", "cmd" , "/k",  "C:\Git\\fala1\\buscador\\telegram_borg_saga.py"], shell=True, executable="C:\windows\system32\cmd.exe")
     subprocess.Popen([ "start", "cmd" , "/k",  "C:\Git\\fala1\\buscador\\telegram_product-sindsct.py"], shell=True, executable="C:\windows\system32\cmd.exe")
 
-    -sindsct.py
+
 
     subprocess.Popen([ "start", "cmd" , "/k",  "C:\Git\\fala1\\buscador\\telegram_ripley60-70.py"], shell=True, executable="C:\windows\system32\cmd.exe")
     subprocess.Popen([ "start", "cmd" , "/k", "C:\Git\\fala1\\buscador\\telegram_ripley70-100.py"], shell=True, executable="C:\windows\system32\cmd.exe")
